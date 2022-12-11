0fdbe2c2585e54e7e9dbb849f80953b398d559c0
tmokmss <tomookam@live.jp>
2022-12-11 19:15:52 +0900

    add cdk code

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,16 +1,33 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { CfnOutput, Duration } from 'aws-cdk-lib';
+import { HttpLambdaIntegration } from '@aws-cdk/aws-apigatewayv2-integrations-alpha';
+import { DockerImageCode, DockerImageFunction } from 'aws-cdk-lib/aws-lambda';
+import { HttpApi } from '@aws-cdk/aws-apigatewayv2-alpha';
+import { Platform } from 'aws-cdk-lib/aws-ecr-assets';
 
 export class CdkStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+  readonly endpoint: string;
 
-    // The code that defines your stack goes here
+  constructor(scope: Construct, id: string, props: cdk.StackProps) {
+    super(scope, id);
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Next.js standaloneを動かすLambdaの定義
+    const handler = new DockerImageFunction(this, 'Handler', {
+      code: DockerImageCode.fromImageAsset('../', {
+        platform: Platform.LINUX_AMD64,
+        exclude: ['cdk'],
+      }),
+      memorySize: 256,
+      timeout: Duration.seconds(30),
+    });
+
+    // Amazon API Gateway HTTP APIの定義
+    const api = new HttpApi(this, 'Api', {
+      apiName: 'Frontend',
+      defaultIntegration: new HttpLambdaIntegration('Integration', handler),
+    });
+
+    new CfnOutput(this, 'ApiEndpoint', { value: api.apiEndpoint });
   }
 }
